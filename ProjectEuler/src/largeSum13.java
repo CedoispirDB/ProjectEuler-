534d48892f74e92f4a1fdcd5c21afbe025e5ba9b
Marco <marcodato03@gmail.com>
2020-09-07 19:05:17 -0400

    First commit

---
+++ ProjectEuler/src/largeSum13.java
@@ -0,0 +1,20 @@
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
+
+public class largeSum13 {
+    public static void sum(List<String> listOfNumbers) {
+        int sum = 0;
+        int x;
+
+
+
+
+        tools.d(sum);
+    }
+
+    public static void main(String[] args) {
+        List<String> listOfNumbers = Arrays.asList("37107287533902102798797998220837590246510135740250", "46376937677490009712648124896970078050417018260538");
+        sum(listOfNumbers);
+    }
+}
