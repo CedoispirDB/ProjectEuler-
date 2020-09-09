62b40e6603c7d59910b903ddebbab6b4f5f8f6c8
Marco <marcodato03@gmail.com>
2020-09-09 12:52:35 -0400

    Project Euler 15 : Using probability

---
+++ ProjectEuler/src/latticePaths.java
@@ -0,0 +1,26 @@
+import java.math.BigInteger;
+
+public class latticePaths {
+    public static BigInteger findPossibilities(long n){
+        long doubleOfN = 2 * n;
+       // tools.d("Double of n: " + doubleOfN);
+
+        BigInteger numerator = tools.factorialOfNumber(doubleOfN);
+       // tools.d("Numerator: " + numerator);
+
+        BigInteger factorialOfN = tools.factorialOfNumber(n);
+       // tools.d("Factorial of n: " + factorialOfN);
+
+        BigInteger denominator = factorialOfN.pow(2);
+       // tools.d("Denominator: " + denominator);
+
+        BigInteger division = numerator.divide(denominator);
+        tools.d("Division: " + division);
+
+        return division;
+    }
+
+    public static void main(String[] args) {
+       findPossibilities(20);
+    }
+}
