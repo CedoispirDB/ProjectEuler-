ec1756b36e53b9e7ecfa28e6f06ce21160a8e92d
Marco <marcodato03@gmail.com>
2020-10-01 14:49:43 -0400

    Project Euler - Problem 19 done

--- ProjectEuler/src/countingSundays19.java
+++ ProjectEuler/src/countingSundays19.java
@@ -1,82 +1,67 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class countingSundays19 {
-    public static int sundayDays() {
-        int day;
-        int month;
-        int year = 1900;
-        int count = 0;
-        int sundays = 0;
-        int x;
-
-        // has to be divisible by 4, but divisible by 100 are not unless divisible by 400.
-        do {
-            //Is a leap year
-            boolean yearDiv4 = (year % 4 == 0);
-            //is not a leap year
-            boolean centuryDiv400 = (year % 100 == 0 && year % 400 != 0);
-            boolean leapYear = yearDiv4 && !centuryDiv400;
-            if (leapYear) {
-                x = 1;
-            } else {
-                x = 0;
-            }
-            month = 1;
-            do {
-                day = 0;
-                if (month == 8) {
-                    do {
-                        if (count % 7 == 0 && day == 1) {
-                            sundays++;
-                        }
-                        day++;
-                        count++;
-
-                    } while (day <= 31);
-
-
-                } else if (month == 2) {
-                    do {
-                        if (count % 7 == 0 && day == 1) {
-                            sundays++;
-
-                        }
-                        day++;
-                        count++;
-
-
-                    } while (day <= 28 + x);
-
-                } else {
-                    if (tools.isItPrime(month)) {
-                        do {
-                            if (count % 7 == 0 && day == 1) {
-                                sundays++;
-                            }
-                            day++;
-                            count++;
-
-                        } while (day <= 31);
-                    } else {
-                        do {
-                            if (count % 7 == 0 && day == 1) {
-                                sundays++;
-
-                            }
-                            day++;
-                            count++;
-
-                        } while (day <= 30);
-                    }
-                }
-                tools.d("count:" + count);
-                month++;
-            } while (month <= 12);
-            year++;
-        } while (year <= 2000);
-
-        return sundays;
+    //Give the day of the week according to the date
+    public static String getDayStringOld(Date date, Locale locale) {
+        DateFormat formatter = new SimpleDateFormat("EEEE", locale);
+        return formatter.format(date);
     }
 
+    //Months with 30 31 28 29
+    //Years divisible by 4
+    //Years divisible by 100 Just are leap year if they are divisible by 400
+
+    //Count the number of sundays
+    //Count from January 1st 1900 to the impute date
+    public static int numberOfSundays(int year, int month, int day) {
+        int yf;
+        int add;
+        int sub;
+
+        int numberOfSundays = 0;
+        for (int y = 1; y <= year - 1900; y++) {
+            yf = y + 1900;
+            boolean yearDiv4 = yf % 4 == 0;
+            boolean centuryDiv = yf % 100 == 0 && yf % 400 == 0;
+            boolean leapYear = yearDiv4 && centuryDiv;
+
+            for (int m = 0; m <= month - 1; m++) {
+                if (m == 0 || m == 2 || m == 4 || m == 6 || m == 7 || m == 8 || m == 9 || m == 11) {
+                    add = 1;
+                    sub = 0;
+                } else if (m == 1) {
+                    if (leapYear) {
+                        sub = 1;
+                    } else {
+                        sub = 2;
+                    }
+                    add = 0;
+                } else {
+                    add = 0;
+                    sub = 0;
+                }
+
+                for (int d = 1; d <= day + add - sub; d++) {
+                    Date date = new Date(y, m, d);
+                    String k = getDayStringOld(date, Locale.US);
+
+                    if (k.equals("Sunday") && d == 1) {
+                        numberOfSundays++;
+                    }
+
+                }
+            }
+        }
+
+        return numberOfSundays;
+    }
+
+
     public static void main(String[] args) {
-        tools.d(sundayDays());
+
+        tools.d("The number of sundays is: " + numberOfSundays(2000,12,30));
     }
 }
