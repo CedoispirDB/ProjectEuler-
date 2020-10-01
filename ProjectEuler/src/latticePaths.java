3af5da16a4b577428ac4c96f7b319ff03d8372e6
Marco <marcodato03@gmail.com>
2020-10-01 14:53:01 -0400

    Project Euler - Problem 15 edited

--- ProjectEuler/src/latticePaths.java
+++ ProjectEuler/src/latticePaths.java
@@ -3,17 +3,9 @@ import java.math.BigInteger;
 public class latticePaths {
     public static BigInteger findPossibilities(long n){
         long doubleOfN = 2 * n;
-       // tools.d("Double of n: " + doubleOfN);
-
         BigInteger numerator = tools.factorialOfNumberB(doubleOfN);
-       // tools.d("Numerator: " + numerator);
-
         BigInteger factorialOfN = tools.factorialOfNumberB(n);
-       // tools.d("Factorial of n: " + factorialOfN);
-
         BigInteger denominator = factorialOfN.pow(2);
-       // tools.d("Denominator: " + denominator);
-
         return numerator.divide(denominator);
     }
 
