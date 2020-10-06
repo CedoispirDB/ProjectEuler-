e837121f3d723039f03bd8aac90edbaeb6c0ffa2
Marco <marcodato03@gmail.com>
2020-10-06 16:11:28 -0400

    Project Euler - Problem 24 done

---
+++ ProjectEuler/src/lexicographicPermutations24.java
@@ -0,0 +1,43 @@
+
+
+public class lexicographicPermutations24 {
+    private static int count = 0;
+
+    //The the number of permutations of a sequence
+    public static int findPermutations(String sequence, String ans, int p) {
+
+        // If string is empty
+        if (sequence.length() == 0) {
+            count++;
+           //System.out.println("Permutation number " + count + " is " + ans + " ");
+            if (count == p) {
+                tools.d(ans);
+
+            }
+            return count;
+
+        }
+
+        for (int n = 0; n < sequence.length(); n++) {
+
+            // ith character of str
+            char digit = sequence.charAt(n);
+
+            // Rest of the string after excluding
+            // the ith character
+            String rest = sequence.substring(0, n) + sequence.substring(n + 1);
+
+            // Recursive call
+            findPermutations(rest, ans + digit, p);
+        }
+
+        return count;
+    }
+
+
+    public static void main(String[] args) {
+        
+        tools.d(findPermutations("0123456789", "", 1000000));
+    }
+
+}
