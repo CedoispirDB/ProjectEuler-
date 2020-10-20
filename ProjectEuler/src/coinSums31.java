335447b318bf1c82eafb7da72e9aefe10a41d4e6
Marco <marcodato03@gmail.com>
2020-10-19 22:05:18 -0400

    Project Euler - Problem 17 done

---
+++ ProjectEuler/src/coinSums31.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class coinSums31 {
+    // 1p, 2p, 5p, 10p, 20p, 50p, £1 (100p), and £2 (200p).
+    // 200p
+    // a + 2b + 5c + 10w + 20x + 50y + 100z = 200
+
+    private static int sumCoins(int p1, int p2, int p5, int p10, int p20, int p50, int p100) {
+        int a = 1;
+        int b = 2;
+        int c = 5;
+        int w = 10;
+        int x = 20;
+        int y = 50;
+        int z = 100;
+        return a * p1 + b * p2 + c * p5 + w * p10 + x * p20 + y * p50 + z * p100;
+    }
+
+    private static int comNum() {
+        int ways = 1;
+
+        for (int p100 = 0; p100 <= 2; p100++) {
+            for (int p50 = 0; p50 <= 4; p50++) {
+                for (int p20 = 0; p20 <= 10; p20++) {
+                    for (int p10 = 0; p10 <= 20; p10++) {
+                        for (int p5 = 0; p5 <= 40; p5++) {
+                            for (int p2 = 0; p2 <= 100; p2++) {
+                                for (int p1 = 0; p1 <= 200; p1++) {
+                                    if (sumCoins(p1, p2, p5, p10, p20, p50, p100) == 200) {
+                                        ways++;
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+        return ways;
+    }
+
+    public static void main(String[] args) {
+        tools.d(comNum());
+    }
+}
--- ProjectEuler/src/numberLetterCounts17.java
+++ ProjectEuler/src/numberLetterCounts17.java
@@ -1,157 +1,102 @@
-import java.awt.image.TileObserver;
 import java.util.HashMap;
 import java.util.Map;
 
 public class numberLetterCounts17 {
+    public static void main(String[] args) {
+        Map<Integer, String> n = new HashMap<>();
+        int lettersSum = 0;
+        n.put(0, "");
+        n.put(1, "one");
+        n.put(2, "two");
+        n.put(3, "three");
+        n.put(4, "four");
+        n.put(5, "five");
+        n.put(6, "six");
+        n.put(7, "seven");
+        n.put(8, "eight");
+        n.put(9, "nine");
+        n.put(10, "ten");
+        n.put(11, "eleven");
+        n.put(12, "twelve");
+        n.put(13, "thirteen");
+        n.put(14, "fourteen");
+        n.put(15, "fifteen");
+        n.put(16, "sixteen");
+        n.put(17, "seventeen");
+        n.put(18, "eighteen");
+        n.put(19, "nineteen");
+        n.put(20, "twenty");
+        n.put(30, "thirty");
+        n.put(40, "forty");
+        n.put(50, "fifty");
+        n.put(60, "sixty");
+        n.put(70, "seventy");
+        n.put(80, "eighty");
+        n.put(90, "ninety");
 
-    public static void counter(int n) {
+        for (int i = 1; i <= 1000; i++) {
+            String text = "";
+            int j = i;
 
-        Map<Integer, String> m = new HashMap<>();
-
-        m.put(0, "and");
-        m.put(1, "one");
-        m.put(2, "two");
-        m.put(3, "three");
-        m.put(4, "four");
-        m.put(5, "five");
-        m.put(6, "six");
-        m.put(7, "seven");
-        m.put(8, "eight");
-        m.put(9, "nine");
-        m.put(10, "ten");
-        m.put(11, "eleven");
-        m.put(12, "twelve");
-        m.put(13, "thirteen");
-        m.put(14, "fourteen");
-        m.put(15, "fifteen");
-        m.put(16, "sixteen");
-        m.put(17, "seventeen");
-        m.put(18, "eighteen");
-        m.put(19, "nineteen");
-        m.put(20, "twenty");
-        m.put(30, "thirty");
-        m.put(40, "forty");
-        m.put(50, "fifty");
-        m.put(60, "sixty");
-        m.put(70, "seventy");
-        m.put(80, "eighty");
-        m.put(90, "ninety");
-        m.put(100, "hundred");
-        m.put(1000, "thousand");
-
-        int count = 1;
-        int addition = 0;
-        long k = n / 1000;
-        long lettersInUnits = 0;
-        long lettersInUnits2 = 0;
-        long lettersInTenths = 0;
-        long lettersInHundreds = 0;
-        long lettersInTeens = 0;
-        long lettersInThousands = 0;
-        long lettersInAnd = 0;
-        long totalLetters;
-
-
-        do {
-            for (int i = 1; i <= 100; i++) {
-                String units2 = m.get(count);
-                lettersInUnits2 += units2.length();
-            }
-            count++;
-        }while(count <= 9 * k);
-
-        tools.d("UnitsInHundreds: " + lettersInUnits2);
-
-
-        //Count how many letters there is in all units for the interval 1-n
-        do {
-            for (int i = 1; i <= 9; i++) {
-                String units = m.get(i);
-                lettersInUnits += units.length();
+            // Thousands
+            if (j >= 1000) {
+                text = getThousandsName(n, i);
+                j = Integer.parseInt(String.valueOf(j).substring(1));
             }
 
-        } while (lettersInUnits != 3240 * k);
-
-        tools.d("Units: " + lettersInUnits);
-
-        //Count how many letters there is in all "teens" numbers for the interval 1-n
-        do {
-            for (int b = 10; b <= 19; b++) {
-                String teens = m.get(b);
-                lettersInTeens += teens.length();
+            // Hundreds
+            if (j < 1000 && j >= 100) {
+                text = text + getHundredsName(n, j);
+                j = Integer.parseInt(String.valueOf(j).substring(1));
             }
 
-        } while (lettersInTeens != 700);
-
-        tools.d("Teens: " + lettersInTeens);
-        tools.d("Sum of 1-19: " + lettersInUnits+lettersInTeens);
-
-        //Count how many letters there is in all the tenths for the interval 1-n
-        do {
-            for (int t = 20; t <= 90; t = t + 10) {
-                String tenths = m.get(t);
-                for (int u = 0; u <= 9; u++) {
-                    lettersInTenths += tenths.length();
-
-                }
+            if (j > 0 && !text.isEmpty()) {
+                text = text + "and ";
             }
 
-        } while (lettersInTenths != 4600 * k);
-
-        tools.d("Tenths: " + lettersInTenths);
-
-        //Count how many letters there is in all the hundreds for the interval 1-n
-        do {
-
-            String hundreds = m.get(100);
-            for (int u = 100; u < 1000; u++) {
-                lettersInHundreds += hundreds.length();
+            // Tens
+            if (j < 100 && j >= 20) {
+                text = text + getTensName(n, j);
+                j = Integer.parseInt(String.valueOf(j).substring(1));
             }
 
+            // Tens
+            if (j < 20) {
+                text = text + n.get(j);
+            }
 
-        } while (lettersInHundreds != 6300 * k);
-
-        tools.d("Hundreds: " + lettersInHundreds);
-
-        //Count how many letters there is in all the thousands for the interval 1-n
-        if (n > 1000) {
-            do {
-
-                String thousands = m.get(1000);
-                for (int u = 1000; u < 1000000; u++) {
-                    lettersInThousands += thousands.length();
-
-                }
-
-
-            } while (lettersInThousands != 72000 * k);
-        } else {
-            addition = 11;
+            tools.d(i + " = " + text + " (" + text.replace(" ", "").length() + ")");
+            lettersSum += text.replace(" ", "").length();
+            tools.d(lettersSum);
         }
 
-        tools.d("Thousands: " + lettersInThousands);
-
-        //Count how many letters there is in all the and's for the interval 1-n
-        do {
-            String and = m.get(0);
-            lettersInAnd += and.length();
-
-
-        } while (lettersInAnd != 2862 * k);
-
-        tools.d("And's: " + lettersInAnd);
-
-        long testSum = lettersInUnits + lettersInUnits2;
-        tools.d(testSum);
-        //tools.d(lettersInUnits);
-        totalLetters = lettersInUnits + lettersInTeens + lettersInTenths + lettersInHundreds + lettersInAnd + addition;
-
-        tools.d("Total letters: " + totalLetters);
 
     }
 
-    public static void main(String[] args) {
-        counter(1000);
+    private static String getThousandsName(Map<Integer, String> n, int x) {
+        String r = "";
+        if (x >= 1000) {
+            r = n.get((int) Math.ceil(x / 1000)) + " thousand ";
+        }
+
+        return r;
     }
 
+    private static String getHundredsName(Map<Integer, String> n, int x) {
+        String r = "";
+        if (x >= 100) {
+            r = n.get((int) Math.ceil(x / 100)) + " hundred ";
+        }
+
+        return r;
+    }
+
+    private static String getTensName(Map<Integer, String> n, int x) {
+        String r = "";
+        if (x >= 20) {
+            r = n.get(Integer.parseInt(String.valueOf(String.valueOf(x).charAt(0))) * 10) + " ";
+        }
+
+        return r;
+    }
 }
