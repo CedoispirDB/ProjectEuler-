fcdeffbd47b5d9698ba7825e92977ea28f771c6c
Marco <marcodato03@gmail.com>
2020-10-31 01:24:33 -0400

    Project Euler - Problem 28

---
+++ ProjectEuler/src/greatestCommonDivisor.java
@@ -0,0 +1,43 @@
+public class greatestCommonDivisor {
+    private static int findGCD(int a, int b) {
+        int gcd = 0;
+        int compare = 0;
+        for (int i = 1; i <= a; i++) {
+            for (int k = 1; k <= b; k++) {
+                if (a % i == 0 && b % k == 0 && i == k) {
+                    if (i > compare) {
+                        gcd = i;
+                        compare = gcd;
+                    }
+
+                }
+            }
+        }
+        return gcd;
+    }
+
+    public static void main(String[] args) {
+        boolean check = true;
+//        for (int a = 1; a <= 1000; a++) {
+//            for (int b = 1; b <= 1000; b++) {
+//                if (tools.isItPrime(a) && !tools.isItPrime(b)) {
+//                    int x = findGCD(a, b);
+//                    //tools.d(findGCD(a, b));
+//                    if (x != 1) {
+//                        check = false;
+//                    }
+//                }
+//            }
+//        }
+
+        tools.d(findGCD(4,9));
+//        if (check) {
+//            tools.d("You are right!!!");
+//        } else {
+//            tools.d("You are wrong :(");
+//        }
+    }
+}
+
+
+
