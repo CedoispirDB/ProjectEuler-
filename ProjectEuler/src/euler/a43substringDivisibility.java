0e570527c7f9b2ae9c6ecc4629f80004fbe270f4
Marco <marcodato03@gmail.com>
2020-11-06 12:58:55 -0500

    Problem 43 done

--- ProjectEuler/src/euler/a43substringDivisibility.java
+++ ProjectEuler/src/euler/a43substringDivisibility.java
@@ -7,11 +7,11 @@ public class a43substringDivisibility {
 
     public static boolean checkIfPandigital(String number) {
         boolean isPan = false;
-        if (number.length() != 10){
+        if (number.length() != 10) {
             return false;
         }
         for (int i = 0; i <= 9; i++) {
-            if (number.contains(String.valueOf(i))){
+            if (number.contains(String.valueOf(i))) {
                 isPan = true;
             } else {
                 isPan = false;
@@ -30,7 +30,7 @@ public class a43substringDivisibility {
     //    d7d8d9=728 is divisible by 13
     //    d8d9d10=289 is divisible by 17
     //Divide values of the substrings of a number
-    private static boolean substringDiv(int num) {
+    private static boolean substringDiv(long num) {
         String numS = String.valueOf(num);
         StringBuilder newString = new StringBuilder(" ");
         int count = 0;
@@ -87,10 +87,11 @@ public class a43substringDivisibility {
     private static long sumOfNum() {
         long sum = 4297667445L;
         for (long i = 1460357290; i <= 9000000000L; i++) {
-            if (i % 1000000000L == 0){
+            if (i % 1000000000L == 0) {
                 tools.d("We are at: " + i + " || with sum:" + sum);
 
-            }            if (checkIfPandigital(String.valueOf(i)) && substringDiv((int) i)) {
+            }
+            if (checkIfPandigital(String.valueOf(i)) && substringDiv(i)) {
                 sum += i;
                 tools.d("i: " + i + " || sum: " + sum);
 
@@ -99,19 +100,21 @@ public class a43substringDivisibility {
         return sum;
     }
 
-// i: 1406357289 sum: 1406357289
+    // i: 1406357289 sum: 1406357289
 // i: 1430952867 sum: 2837310156
 // i: 1460357289 sum: 4297667445
     public static void main(String[] args) {
-//        tools.d(sumOfNum());
+        tools.d(sumOfNum());
         //1406357289
-        for (long i = 4000000000L; i <= 5000000000L; i++) {
+//        for (long i = 4000000000L; i <= 5000000000L; i++) {
+//
+//            tools.d("For: " + i +": " + substringDiv((int) i ));
+//
+//
+//        }
 
-            tools.d("For: " + i +": " + substringDiv((int) i ));
-
-
-        }
-        ;
+        //4195091684
+//        tools.d( substringDiv((int)  4 195 091 685L));
         //tools.d(checkIfPandigital("4000000000"));
     }
 
